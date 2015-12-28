1bebdb2d6cf4311d2f55126f9dbe076e0ab46d36
mengqiang <mengqiang81@gmail.com>
2015-12-28 21:09:22 +0800

    add java 8 javatime support

--- src/main/groovy/org/nofdev/http/ObjectMapperFactory.groovy
+++ src/main/groovy/org/nofdev/http/ObjectMapperFactory.groovy
@@ -3,16 +3,19 @@ import com.fasterxml.jackson.databind.DeserializationFeature
 import com.fasterxml.jackson.databind.ObjectMapper
 import com.fasterxml.jackson.databind.SerializationFeature
 import com.fasterxml.jackson.datatype.joda.JodaModule
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule
+
 /**
  * Created by Qiang on 11/11/15.
  */
 class ObjectMapperFactory {
-    public static ObjectMapper createObjectMapper() {
-        ObjectMapper objectMapper = new ObjectMapper();
-        objectMapper.registerModule(new JodaModule());
-        objectMapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true);
-        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false);
-        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS,false);
-        return objectMapper;
+    static ObjectMapper createObjectMapper() {
+        ObjectMapper objectMapper = new ObjectMapper()
+        objectMapper.registerModule(new JodaModule())
+        objectMapper.registerModule(new JavaTimeModule())
+        objectMapper.configure(DeserializationFeature.ACCEPT_SINGLE_VALUE_AS_ARRAY, true)
+        objectMapper.configure(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES, false)
+        objectMapper.configure(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS,false)
+        return objectMapper
     }
 }
